e8e6932041d63bbbda1936a8c48dbeb84933e48d
Marlin <marlinreams@gmail.com>
2022-11-21 16:51:20 -0600

    got user post/get working

--- sample_service/queries/users.py
+++ sample_service/queries/users.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from fastapi import Response
 from queries.client import Queries
 
 class UserIn(BaseModel):
@@ -20,10 +21,20 @@ class UserQueries(Queries):
     DB_NAME = "user"
     COLLECTION = "users"
 
-    def create(self, info: UserIn):
+    def create(self, info = UserIn, response_model = UserOut):
         props = info.dict()
 
         try:
             self.collection.insert_one(props)
         except:
             pass
+        props["id"] = str(props["_id"])
+        return UserOut(**props)
+
+    def get_all_users(self):
+        users = []
+        props = self.collection.find({})
+        for document in props:
+            document["id"] = str(document["_id"])
+            users.append(UserOut(**document))
+        return users
--- sample_service/routers/users.py
+++ sample_service/routers/users.py
@@ -1,17 +1,28 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, Request
 from queries.users import UserIn, UserOut, UserQueries
 
 
 router = APIRouter()
 
 
-@router.post("/api/users")
+@router.post("/api/users", response_model = UserOut)
 async def create_account(
     info: UserIn,  # this is what should be in the body
     users: UserQueries = Depends(),
 ):
+
     try:
-        users.create(info)
+       info = users.create(info)
     except:
         pass
-    return UserOut
+    print("USER IN::::::", UserIn)
+    print("USER OUT::::::", UserOut)
+
+    return info
+
+@router.get("/api/users")
+def get_user(
+    response: Response, users: UserQueries = Depends()
+):
+    response = users.get_all_users()
+    return response
