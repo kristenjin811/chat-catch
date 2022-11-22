cea3fda24337b54490a6f1a6b869579c84d99be9
Marlin <marlinreams@gmail.com>
2022-11-21 19:15:08 -0600

    added message endpoints

--- sample_service/main.py
+++ sample_service/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from routers import users
+from routers import users, messages
 
 
 app = FastAPI()
 app.include_router(users.router)
+app.include_router(messages.router)
 
 
 app.add_middleware(
