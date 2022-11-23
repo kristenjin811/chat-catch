74b4c0cd6f19b1453f719bfec044ce047119e199
Jared-Greathouse <a.jaredgreathouse@gmail.com>
2022-11-23 15:53:46 -0800

    uvicorn now running on 8001 in accounts

--- chat/db.py
+++ chat/db.py
@@ -1,11 +1,11 @@
-
 class MessagesIn(BaseModel):
-  message: str
-  from_time:
+    message: str
+    from_time: str
+
 
 class MessagesQueries:
-  def get_all_messages(self):
-    result = list(db.messages.find())
-    for message in result:
-      message["id"] = message["_id"]
-    return result
+    def get_all_messages(self):
+        result = list(db.messages.find())
+        for message in result:
+            message["id"] = message["_id"]
+        return result
