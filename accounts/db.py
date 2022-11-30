c78db7fd4c7a4695c05e2944e09374d494b752ef
Marlin Reams <marlinreams@gmail.com>
2022-11-30 20:39:01 +0000

    Revert "Merge branch 'main'"

--- accounts/db.py
+++
@@ -1,8 +0,0 @@
-import os
-import pymongo
-
-
-client = pymongo.MongoClient(os.environ["DATABASE_URL"])
-dbname = os.environ["DATABASE_NAME"]
-# host_info = mongo_client["HOST"]
-db = client[dbname]
