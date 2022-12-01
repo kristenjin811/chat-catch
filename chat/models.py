e6258657a7defc33b1c8495ee607882da12f439b
Jared-Greathouse <a.jaredgreathouse@gmail.com>
2022-12-01 08:17:10 -0800

    flake8 passing after file tree restructure

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,5 @@
 # this models.py file holds all the models
-from fastapi import Response
+# from fastapi import Response
 from pydantic import BaseModel, Field
 from typing import List, Optional
 from bson import ObjectId
@@ -71,43 +71,43 @@ class MessagesIn(BaseModel):
     from_time: str
 
 
-class MessagesQueries:
-    def get_all_messages(self):
-        result = list(db.messages.find())
-        for message in result:
-            message["id"] = message["_id"]
-        return result
+# class MessagesQueries:
+#     def get_all_messages(self):
+#         # result = list(db.messages.find())
+#         for message in result:
+#             message["id"] = message["_id"]
+#         # return result
 
 
-class MessageQueries(Queries):
-    DB_NAME = "message"
-    COLLECTION = "messages"
+# class MessageQueries(Queries):
+#     DB_NAME = "message"
+#     COLLECTION = "messages"
 
-    def create(self, info=MessageIn, response_model=MessageOut):
-        props = info.dict()
+#     def create(self, info=MessageIn, response_model=MessageOut):
+#         props = info.dict()
 
-        try:
-            self.collection.insert_one(props)
-        except:
-            pass
-        props["id"] = str(props["_id"])
-        return MessageOut(**props)
+#         try:
+#             self.collection.insert_one(props)
+#         except:
+#             pass
+#         props["id"] = str(props["_id"])
+#         return MessageOut(**props)
 
-    def get_all_messages(self):
-        messages = []
-        props = self.collection.find({})
-        for document in props:
-            document["id"] = str(document["_id"])
-            messages.append(MessageOut(**document))
-        return messages
+#     def get_all_messages(self):
+#         messages = []
+#         props = self.collection.find({})
+#         for document in props:
+#             document["id"] = str(document["_id"])
+#             messages.append(MessageOut(**document))
+#         return messages
 
-    def get_message(self, id):
-        message = self.collection.find_one({"_id": id})
-        message["id"] = str(message["_id"])
-        return message
+#     def get_message(self, id):
+#         message = self.collection.find_one({"_id": id})
+#         message["id"] = str(message["_id"])
+#         return message
 
-    def delete_message(self, id):
-        self.collection.delete_one({"_id": id})
+#     def delete_message(self, id):
+#         self.collection.delete_one({"_id": id})
 
 
 class Message(BaseModel):
@@ -132,27 +132,27 @@ class ChatroomOut(BaseModel):
     name: str
 
 
-class ChatroomQueries(Queries):
-    DB_NAME = "chatroom"
-    COLLECTION = "chatrooms"
+# class ChatroomQueries(Queries):
+#     DB_NAME = "chatroom"
+#     COLLECTION = "chatrooms"
 
-    def create(self, info=ChatroomIn, response_model=ChatroomOut):
-        props = info.dict()
-        try:
-            self.collection.insert_one(props)
-        except:
-            pass
-        props["id"] = str(props["_id"])
-        return ChatroomOut(**props)
+#     def create(self, info=ChatroomIn, response_model=ChatroomOut):
+#         props = info.dict()
+#         try:
+#             self.collection.insert_one(props)
+#         except:
+#             pass
+#         props["id"] = str(props["_id"])
+#         return ChatroomOut(**props)
 
-    def get_all(self, info=ChatroomIn, response_model=ChatroomOut):
-        props = info.dict()
-        try:
-            self.collection.insert_one(props)
-        except:
-            pass
-        props["id"] = str(props["_id"])
-        return ChatroomOut(**props)
+#     def get_all(self, info=ChatroomIn, response_model=ChatroomOut):
+#         props = info.dict()
+#         try:
+#             self.collection.insert_one(props)
+#         except:
+#             pass
+#         props["id"] = str(props["_id"])
+#         return ChatroomOut(**props)
 
 
 class Room(BaseModel):
