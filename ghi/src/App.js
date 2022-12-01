0a960ce36379ecedf7523dea20283fdde6555a1f
Marlin <marlinreams@gmail.com>
2022-12-01 16:31:38 -0600

    updated readme

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './chat-logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Where the world connects.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Where do you think this will take you?
         </a>
       </header>
     </div>
