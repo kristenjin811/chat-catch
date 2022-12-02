bd981697b7bceaab1aee1ec646cf2682632ef681
Jared-Greathouse <a.jaredgreathouse@gmail.com>
2022-12-02 10:15:43 -0800

    flake8 on reference_code

--- reference_code/poller2.py
+++ reference_code/poller2.py
@@ -5,19 +5,26 @@ import time
 
 import pymongo
 import requests
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from fastapi import APIRouter, Depends, Response
+
+# from bson.json_util import dumps
+# from bson.objectid import ObjectId
+from fastapi import (
+    APIRouter,
+    # Depends,
+    # Response
+)
+
 # from queries.poller import PollerIn, PollerOut, PollerQueries
 # from queries.users import UserQueries
 
 router = APIRouter()
 
+
 MONGO_URL = os.environ.get("DATABASE_URL")
 client = pymongo.MongoClient(MONGO_URL)
 
-@router.get("/api/chat/userVO")
 
+@router.get("/api/chat/userVO")
 def getting_users_to_puller():
     response = requests.get("http://localhost:8000/api/users")
     content = json.loads(response.content)
@@ -32,12 +39,13 @@ def getting_users_to_puller():
 
 def poll():
     while True:
-        print('Checking for any new users...')
+        print("Checking for any new users...")
         try:
             getting_users_to_puller()
         except Exception as e:
             print(e, file=sys.stderr)
         time.sleep(10)
+
 
 if __name__ == "__main__":
     poll()
