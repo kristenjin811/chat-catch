1d045d0735b5b84aa0b255274dc549f34d26d5c2
Marlin <marlinreams@gmail.com>
2022-12-07 03:34:25 -0600

    trying to get auth going

--- chat/queries/accounts.py
+++ chat/queries/accounts.py
@@ -1,30 +1,70 @@
 from pydantic import BaseModel
+from typing import Optional
+import pymongo
+import os
+from pymongo.errors import DuplicateKeyError
+from config import MONGODB_DB_NAME
+from mongodb import get_nosql_db
+from pymongo import MongoClient
+from fastapi import (
+    Depends,
+)
+
+
+
+
+class Account(BaseModel):
+    id: str
+    email: str
+    hashed_password: str
+
+
+class AccountIn(BaseModel):
+    email: str
+    password: str
+
+
+class AccountOut(BaseModel):
+    id: str
+    email: str
 
 
 class DuplicateAccountError(ValueError):
     pass
 
 
+class AccountRepository:
+    async def get_one(self, email: str) -> Optional[Account]:
+        client = await get_nosql_db()
+        db = client[MONGODB_DB_NAME]
 
-class AccountIn(BaseModel):
-    email: str
-    password: str
-    full_name: str
-
-
-class AccountOut(BaseModel):
-    id: str
-    email: str
-    full_name: str
-
-class AccountOutWithPassword(AccountOut):
-    hashed_password: str
+        print("db ACCOUNT QUERIES.PY :::::::::", db)
 
 
-class AccountQueries():
+        result = db.accounts.find_one({"email": email})
+        if result:
+            result["id"] = str(result["_id"])
+        return Account(**result)
 
-    def get(self, email: str):
-        return AccountOutWithPassword
+    async def create(self, account: AccountIn, hashed_password: str) -> Account:
+        print("This is queries create   ::::::::::::::::::::::::::")
+        client: MongoClient = Depends(get_nosql_db)
+        print("client ACCOUNT QUERIES.PY:::::::::::::::::::::::::::::::::::", client)
 
-    def create(self, info: AccountIn, hashed_password: str):
-        return AccountOutWithPassword
+        db = await client[MONGODB_DB_NAME]
+
+
+        print("db ACCOUNT QUERIES.PY::::::::::::::::",db)
+
+        props = account.dict()
+
+        props["hashed_password"] = hashed_password
+
+        props.pop("password")
+        try:
+
+            db.accounts.insert_one(props)
+        except DuplicateKeyError:
+            raise DuplicateAccountError()
+        props["id"] = str(props["_id"])
+        return Account(**props)
--- chat/routers/accounts.py
+++ chat/routers/accounts.py
@@ -14,38 +14,76 @@ from pydantic import BaseModel
 from queries.accounts import (
     AccountIn,
     AccountOut,
-    AccountQueries,
+    AccountRepository,
     DuplicateAccountError,
 )
+
 
 class AccountForm(BaseModel):
     username: str
     password: str
 
+
 class AccountToken(Token):
     account: AccountOut
+
 
 class HttpError(BaseModel):
     detail: str
 
+
 router = APIRouter()
 
 
-@router.post("/api/accounts", response_model=AccountToken | HttpError)
+@router.post("/accounts", response_model=AccountToken | HttpError)
 async def create_account(
+    
     info: AccountIn,
     request: Request,
     response: Response,
-    accounts: AccountQueries= Depends(),
-):
+    repo: AccountRepository = Depends(),
+    ):
+    print("info::::::::", info)
+    print("request::::::::", request)
+    print("response::::::::", response)
+    print("repo::::::::", repo)
     hashed_password = authenticator.hash_password(info.password)
-    try:
-        account = accounts.create(info, hashed_password)
-    except DuplicateAccountError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Cannot create an account with those credentials",
-        )
+    print("hashed_password:::::::", hashed_password)
+    # try:
+    #     account = repo.create(info, hashed_password)
+    # except DuplicateAccountError:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         detail="Error, email already in use",
+    #     )
+    account = repo.create(info, hashed_password)
+    print("account router::::::::::", account)
     form = AccountForm(username=info.email, password=info.password)
-    token = await authenticator.login(response, request, form, accounts)
+    print("form:::::::::::::", form)
+    token = await authenticator.login(response, request, form, repo)
+    print("token::::::::::::", token)
     return AccountToken(account=account, **token.dict())
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: AccountOut = Depends(authenticator.try_get_current_account_data),
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
+
+# @router.get("/gathering/accounts/{email}", response_model=[Account])
+# def get_one_account(
+#   email: str,
+#   response: Response,
+#   repo: AccountRepository = Depends(),
+# ) -> Account:
+#   account = repo.get_one(email)
+#   if account is None:
+#     response.status_code = 404
+#   return account
