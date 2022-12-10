a5ca5e7ad00914d7fd44c7ed53ebd5f592050835
Jared-Greathouse <a.jaredgreathouse@gmail.com>
2022-12-10 12:24:44 -0800

    added upload user to members list controller

--- chat/controllers/chatrooms.py
+++ chat/controllers/chatrooms.py
@@ -30,12 +30,15 @@ async def upload_message_to_chatroom(data):
     client = await get_nosql_db()
     db = client[MONGODB_DB_NAME]
     try:
+        current_user = message_data["user_name"]
         chatroom_name = message_data["chatroom_name"]
         chatroom = await get_chatroom(chatroom_name)
         message_body = {
             "user_name": message_data["user_name"],
             "content": message_data["content"],
         }
+        if current_user not in chatroom["members"]:
+            await upload_member_to_chatroom(current_user, chatroom)
         collection = db.chatrooms
         collection.update_one(
             {"_id": ObjectId(chatroom["_id"])},
@@ -44,6 +47,24 @@ async def upload_message_to_chatroom(data):
         return True
     except Exception as e:
         logger.error(f"error adding message to DB: {type(e)}{e}")
+        return False
+
+
+async def upload_member_to_chatroom(current_user, chatroom):
+    client = await get_nosql_db()
+    db = client[MONGODB_DB_NAME]
+    try:
+        new_member = {"user_name": current_user}
+        collection = db.chatrooms
+        collection.update_one(
+            {"_id": ObjectId(chatroom["_id"])},
+            {"$push": {"members": new_member}},
+        )
+        return True
+    except Exception as e:
+        logger.error(
+            f"error adding the user to members_list in DB: {type(e)}{e}"
+        )
         return False
 
 
