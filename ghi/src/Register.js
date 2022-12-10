9dae1698eb5b6729eb507d159598a27655ee0f0e
Wenqi Jin <kristenjinn@gmail.com>
2022-12-10 14:35:36 -0800

    fixed register.js

--- ghi/src/Register.js
+++ ghi/src/Register.js
@@ -5,11 +5,12 @@ import { useToken } from "./GetToken";
 
 
 export default function Register() {
-  const [, , ] = useToken();
+  const [, , , signup] = useToken();
 
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
   const [name, setName] = useState('');
+  const [invalid, setInvalid] = useState(false);
   const navigate = useNavigate();
 
   const clearState = () => {
@@ -17,8 +18,10 @@ export default function Register() {
     setPass("");
     setName("");
   };
+  console.log("heyyy")
   async function handleSubmit(e) {
     e.preventDefault();
+    const successful = await signup(email, name, pass);
     clearState();
     navigate("/chats/");
   }
@@ -38,6 +41,15 @@ export default function Register() {
               <button type="submit" className="btn btn-dark btn-block btn-large">Register</button>
             </form>
             <Link className="login" to="/">Login</Link>
+            {invalid && (
+              <div
+                className="alert alert-danger mb-0 p-4 mt-4"
+                id="invalid-message"
+              >
+                The email you've entered is already in use. Please log in or
+                try again.
+              </div>
+            )}
           </div>
       </div>
     </>
