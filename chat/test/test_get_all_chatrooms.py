48d95a06be8222d54a7e3ec7d4ba230efe2d50af
Jared-Greathouse <a.jaredgreathouse@gmail.com>
2022-12-12 21:29:30 +0000

    I'm too old for this ish...

--- chat/test/test_get_all_chatrooms.py
+++ chat/test/test_get_all_chatrooms.py
@@ -13,7 +13,7 @@ class EmptyChatroomQueries:
 async def test_get_all_chatrooms():
     app.dependency_overrides[ChatroomQueries] = EmptyChatroomQueries
     async with AsyncClient(
-        app=app, 
+        app=app,
         base_url="https://{process.env.REACT_APP_CHAT_API_HOST}/api",
     ) as ac:
         response = await ac.get("/chatrooms")
